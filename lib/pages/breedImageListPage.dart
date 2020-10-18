ac0735b436ad3d9b79e2d95c78213c34bce9464f
Samuell324 <chistov_artem@mail.ru>
2020-10-18 15:48:30 +0400

    1.1.2 Implement breedImageListPage basic UI

---
+++ lib/pages/breedImageListPage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ImageListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp (
+      home: Scaffold (
+        appBar: AppBar (
+          title: Text('Breed image list page'),
+        ),
+      ),
+    );
+  }
+}
